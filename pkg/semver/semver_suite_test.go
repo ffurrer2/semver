ce4c2fd4e2bd4bbfb8f0c3016f3e0b71e90b57f7
Felix Furrer <ffurrer2@users.noreply.github.com>
2021-01-09 19:14:50 +0100

    Refactor tests

--- pkg/semver/semver_suite_test.go
+++ pkg/semver/semver_suite_test.go
@@ -2,27 +2,15 @@
 package semver_test
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 
-	"github.com/onsi/ginkgo/config"
-	"github.com/onsi/ginkgo/reporters"
-
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/config"
 	. "github.com/onsi/gomega"
 )
 
 func TestSemver(t *testing.T) {
 	RegisterFailHandler(Fail)
-	resultDir, ok := os.LookupEnv("TEST_RESULT_DIR")
-	if !ok {
-		resultDir = "../../artifacts/test-results"
-	}
-	if err := os.MkdirAll(resultDir, os.ModePerm); err != nil {
-		panic(err)
-	}
-	junitReporter := reporters.NewJUnitReporter(filepath.Join(resultDir, "semver-tests-junit-report.xml"))
 	config.DefaultReporterConfig.Verbose = true
-	RunSpecsWithDefaultAndCustomReporters(t, "Semver Suite", []Reporter{junitReporter})
+	RunSpecs(t, "Semver Suite")
 }
